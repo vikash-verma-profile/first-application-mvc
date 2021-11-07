dc6623948b110d25068b7aa8f590c2140e9ddcea
vikash-verma-profile <er.vikashverma@yahoo.com>
2021-11-07 13:21:27 +0400

    first commit

---
+++ FirstApplication/FirstApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,45 @@
+﻿using FirstApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstApplication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+
+        //this is added to store employees
+        static List<Employee> employees=new List<Employee>();
+        public ActionResult Index(Employee employee)
+        {
+
+            if(employee.EmployeeName!=null)
+            {
+                employees.Add(employee);
+                return RedirectToAction("EmployeeList");
+            }
+
+            //this is added to update submit button value
+            ViewBag.buttonName = "Create";
+            return View();
+        }
+
+        public ActionResult EmployeeList()
+        {
+            return View(employees);
+        }
+        
+        public ActionResult Edit(int id)
+        {
+            
+            var employee = employees.Where(x => x.EmployeeID == id).FirstOrDefault();
+            //this is added to update submit button value
+            ViewBag.buttonName = "Update";
+            TempData["update"] = true;
+            return View("Index", employee);
+        }
+    }
+}
---
+++ FirstApplication/FirstApplication/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstApplication.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+
+    }
+}
